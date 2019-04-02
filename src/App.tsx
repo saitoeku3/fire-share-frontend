1eb6294224f48a6537b50b5fcc424a92d9d5946c
saitoeku3 <saitoeku3@gmail.com>
2019-04-02 22:46:03 +0900

    Improve appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
 import React, { useCallback, useContext, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components'
 import { RootContext, uploadFile } from './context'
 
 const App = () => {
   const { state, dispatch } = useContext(RootContext)
-  const [file, setFile] = useState<FormData>(null as any)
+  const [formData, setFormData] = useState<FormData>(null as any)
 
-  const fileLink = state.url ? <a href={state.url}>{state.url}</a> : ''
-
-  // Dropzone
-  const onDrop = useCallback((acceptedFiles: any) => {
-    const [file] = acceptedFiles
-    const formData = new FormData()
-    formData.append('file', file)
-    setFile(formData)
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const [inputFile] = acceptedFiles
+    const fd = new FormData()
+    fd.append('file', inputFile)
+    setFormData(fd)
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-  const message = isDragActive ? 'Drop the files here ...' : 'Drop or click here!'
 
   return (
-    <>
+    <div style={{ textAlign: 'center' }}>
       <h1>Fire Share</h1>
-      <div {...getRootProps()}>
+      <Dropzone {...getRootProps()}>
         <input {...getInputProps()} />
-        <p style={{ margin: 0 }}>{message}</p>
-      </div>
-      <button onClick={() => uploadFile(file)(dispatch)}>send</button>
-      <div>{fileLink}</div>
-    </>
+        <p style={{ margin: 'auto' }}>
+          {isDragActive
+            ? 'Drop the file here ...'
+            : 'Drag and drop file here, or click to select file'}
+        </p>
+      </Dropzone>
+      <UploadButton onClick={() => uploadFile(formData)(dispatch)}>upload</UploadButton>
+      {state.url && <p>Share Link: {state.url}</p>}
+    </div>
   )
 }
 
 export default App
+
+const Dropzone = styled.div`
+  align-items: center;
+  background-color: #ddd;
+  border: dashed black;
+  cursor: pointer;
+  display: flex;
+  height: 40vh;
+  margin: 0 auto 32px auto;
+  width: 70vw;
+`
+
+const UploadButton = styled.button`
+  cursor: pointer;
+  font-size: 16px;
+  height: 32px;
+  width: 128px;
+`
