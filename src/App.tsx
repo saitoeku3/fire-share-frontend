3d291ba0b4dc40bdc083a1837f82fade3246df76
saitoeku3 <saitoeku3@gmail.com>
2019-04-02 16:46:05 +0900

    Add file upload area using dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import React, { useContext, useState } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
 import { RootContext, uploadFile } from './context'
 
 const App = () => {
   const { state, dispatch } = useContext(RootContext)
   const [file, setFile] = useState<FormData>(null as any)
 
-  const changeFile = (event: any) => {
-    const [inputFile] = event.target.files
-    const formData = new FormData()
-    formData.append('file', inputFile)
-    setFile(formData)
-  }
-
   const fileLink = state.url ? <a href={state.url}>{state.url}</a> : ''
+
+  // Dropzone
+  const onDrop = useCallback((acceptedFiles: any) => {
+    const [file] = acceptedFiles
+    const formData = new FormData()
+    formData.append('file', file)
+    setFile(formData)
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  const message = isDragActive ? 'Drop the files here ...' : 'Drop or click here!'
 
   return (
     <>
-      <input type="file" onChange={changeFile} />
+      <h1>Fire Share</h1>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <p style={{ margin: 0 }}>{message}</p>
+      </div>
       <button onClick={() => uploadFile(file)(dispatch)}>send</button>
       <div>{fileLink}</div>
     </>
